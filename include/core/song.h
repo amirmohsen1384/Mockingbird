bb7d4d4ef30ba5a8d9045ccb48f3ccc6fd9791a7
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-26 01:23:19 +0330

    Defined a song
    Song can be used to work with the meta-data of a song.

---
+++ include/core/song.h
@@ -0,0 +1,62 @@
+#ifndef SONG_H
+#define SONG_H
+
+#include <QUrl>
+#include <QPixmap>
+#include <QVariant>
+#include "general.h"
+
+class Song
+{
+public:
+    enum Genre
+    {
+        Rock,
+        Rap,
+        Pop,
+        Jazz,
+        Electronic,
+        Classic,
+        Folk,
+        Country
+    };
+
+public:
+    Genre getGenre() const;
+    QUrl getAddress() const;
+    QString getName() const;
+    QPixmap getCover() const;
+    QString getArtist() const;
+    int getPublicationYear() const;
+    
+    bool operator==(const Song &other) const;
+    bool operator!=(const Song &other) const;
+
+    friend QDataStream& operator<<(QDataStream &stream, const Song &another);
+    friend QDataStream& operator>>(QDataStream &stream, Song &another);
+
+public:
+    void setArtist(const QString &value);
+    void setCover(const QPixmap &value);
+    void setName(const QString &value);
+    void setPublicationYear(int value);
+    void setAddress(const QUrl &value);
+    void setGenre(Genre value);
+
+private:
+    QUrl address;
+    QString name;
+    QPixmap cover;
+    QString artist;
+    int publicationYear;
+    Genre genre = Genre::Classic;
+};
+
+QDataStream& operator<<(QDataStream &stream, const Song &another);
+QDataStream& operator>>(QDataStream &stream, Song &another);
+
+Q_DECLARE_METATYPE(Song)
+
+using SongList = QList<Song>;
+
+#endif // SONG_H
---
+++ src/core/song.cpp
@@ -0,0 +1,115 @@
+#include "include/core/song.h"
+#include <QDataStream>
+
+QString Song::getName() const
+{
+    return name;
+}
+
+QPixmap Song::getCover() const
+{
+    return cover;
+}
+
+Song::Genre Song::getGenre() const
+{
+    return genre;
+}
+
+QString Song::getArtist() const
+{
+    return artist;
+}
+
+int Song::getPublicationYear() const
+{
+    return publicationYear;
+}
+
+bool Song::operator==(const Song &other) const
+{
+    if(this->name != other.name)
+    {
+        return false;
+    }
+    else if(this->genre != other.genre)
+    {
+        return false;
+    }
+    else if(this->artist != other.artist)
+    {
+        return false;
+    }
+    else if(this->publicationYear != other.publicationYear)
+    {
+        return false;
+    }
+    else if(this->address != other.address)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+bool Song::operator!=(const Song &other) const
+{
+    return !(*this == other);
+}
+
+QUrl Song::getAddress() const
+{
+    return address;
+}
+
+void Song::setName(const QString &value)
+{
+    name = value;
+}
+
+void Song::setGenre(Genre value)
+{
+    genre = value;
+}
+void Song::setArtist(const QString &value)
+{
+    artist = value;
+}
+
+void Song::setCover(const QPixmap &value)
+{
+    cover = value;
+}
+
+void Song::setPublicationYear(int value)
+{
+    publicationYear = value;
+}
+void Song::setAddress(const QUrl &value)
+{
+    address = value;
+}
+
+QDataStream& operator<<(QDataStream &stream, const Song &another)
+{
+    stream << another.name;
+    stream << another.genre;
+    stream << another.cover;
+    stream << another.artist;
+    stream << another.address;
+    stream << another.publicationYear;
+    return stream;
+}
+
+QDataStream& operator>>(QDataStream &stream, Song &another)
+{
+    stream >> another.name;
+    stream >> another.genre;
+    stream >> another.cover;
+    stream >> another.artist;
+    stream >> another.address;
+    stream >> another.publicationYear;
+    return stream;
+}
