4389e97fe51e7b68f9cf23a5d1b2d03547cd4a92
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-05-29 13:53:32 +0330

    Completed the artist meta data class

--- include/core/artist.h
+++ include/core/artist.h
@@ -2,12 +2,14 @@
 #define ARTIST_H
 
 #include <QImage>
+#include "general.h"
 
-class Artist
+class Artist : public Entity
 {
 public:
     enum Role
     {
+        KeyRole = Qt::UserRole,
         NameRole = Qt::DisplayRole,
         PhotoRole = Qt::UserRole + 2,
         BiographyRole = Qt::UserRole + 1,
@@ -26,8 +28,17 @@ public:
     bool operator==(const Artist &other) const;
     bool operator!=(const Artist &other) const;
 
+    virtual void saveToRecord(const IDContainer &value) const override;
+
     friend QDataStream& operator>>(QDataStream &stream, Artist &target);
     friend QDataStream& operator<<(QDataStream &stream, const Artist &target);
+
+    static Artist loadFromRecord(const IDContainer &value);
+    static IDs loadIDsFromRecord(const IDContainer &value);
+    static void saveIDsToRecord(const IDs &idList, const IDContainer &value);
+
+private:
+    static QDir createRecord(const IDContainer &value);
 
 private:
     QString name;
--- src/core/artist.cpp
+++ src/core/artist.cpp
@@ -55,6 +55,121 @@ bool Artist::operator!=(const Artist &other) const
     return !(*this == other);
 }
 
+void Artist::saveToRecord(const IDContainer &value) const
+{
+    const QString &target = QString::number(value);
+    QDir folder = MainFolder::getArtists();
+    if(!folder.cd(target))
+    {
+        folder = createRecord(value);
+    }
+    QFile file(folder.absoluteFilePath("metadata.dat"));
+    if(!file.open(QFile::WriteOnly))
+    {
+        return;
+    }
+    QDataStream stream(&file);
+    stream << *this;
+}
+
+void Artist::saveIDsToRecord(const IDs &idList, const IDContainer &value)
+{
+    if(!ID::isValid(value))
+    {
+        return;
+    }
+    const QString target = QString::number(value);
+    QDir folder = MainFolder::getArtists();
+    if(!folder.cd(target))
+    {
+        folder = createRecord(value);
+    }
+    QFile file(folder.absoluteFilePath("keys.dat"));
+    if(!file.open(QFile::WriteOnly))
+    {
+        return;
+    }
+    QDataStream stream(&file);
+    stream << idList;
+}
+
+Artist Artist::loadFromRecord(const IDContainer &value)
+{
+    if(!ID::isValid(value))
+    {
+        return Artist();
+    }
+    QDir result = MainFolder::getArtists();
+    if(!result.cd(QString::number(value)))
+    {
+        return Artist();
+    }
+    QFile file(result.absoluteFilePath("metadata.dat"));
+    if(!file.open(QFile::ReadOnly))
+    {
+        return Artist();
+    }
+    Artist data;
+    QDataStream stream(&file);
+    if((stream >> data).status() == QDataStream::Ok)
+    {
+        data.valid = true;
+        return data;
+    }
+    else
+    {
+        return Artist();
+    }
+}
+
+IDs Artist::loadIDsFromRecord(const IDContainer &value)
+{
+    if(!ID::isValid(value))
+    {
+        return IDs();
+    }
+    QDir folder = MainFolder::getArtists();
+    if(!folder.cd(QString::number(value)))
+    {
+        return IDs();
+    }
+    QFile file(folder.absoluteFilePath("keys.dat"));
+    if(!file.open(QFile::ReadOnly))
+    {
+        return IDs();
+    }
+    IDs idList;
+    QDataStream stream(&file);
+    if((stream >> idList).status() != QDataStream::Ok)
+    {
+        return IDs();
+    }
+    return idList;
+}
+
+QDir Artist::createRecord(const IDContainer &value)
+{
+    const QString &target = QString::number(value);
+    QDir result = MainFolder::getArtists();
+    result.mkdir(target);
+    result.cd(target);
+    {
+        QFile metaFile(result.absoluteFilePath("metadata.dat"));
+        if(!metaFile.exists())
+        {
+            metaFile.open(QFile::WriteOnly);
+        }
+    }
+    {
+        QFile keyFile(result.absoluteFilePath("keys.dat"));
+        if(!keyFile.exists())
+        {
+            keyFile.open(QFile::WriteOnly);
+        }
+    }
+    return result;
+}
+
 QDataStream& operator>>(QDataStream &stream, Artist &target)
 {
     stream >> target.name;
