e32be1217f0342a1dfa720e1b03bb742818efe66
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-06-06 11:47:57 +0330

    Completed the welcome panel

--- include/panels/welcomepanel.h
+++ include/panels/welcomepanel.h
@@ -1,26 +1,43 @@
 #ifndef WELCOMEPANEL_H
 #define WELCOMEPANEL_H
 
-#include <QMainWindow>
+#include <QDialog>
+#include "include/core/general.h"
 
 namespace Ui
 {
     class WelcomePanel;
 }
 
-class WelcomePanel : public QMainWindow
+class WelcomePanel : public QDialog
 {
     Q_OBJECT
 
 public:
+    enum LoginMode {NoLogin, LoginAsAdmin, LoginAsUser};
+
     Q_DISABLE_COPY_MOVE(WelcomePanel)
     explicit WelcomePanel(QWidget *parent = nullptr);
     ~WelcomePanel();
 
+    LoginMode getLoginMode() const;
+    IDContainer getSignedkey() const;
+
 public slots:
     void goToSigninPage();
+    void goToAccountPage();
+
+    virtual void accept() override;
+
+    void toggleShowLoginPassword(bool value);
+    void toggleShowAccountPassword(bool value);
+
+    void rejectCreation();
+    void acceptCreation();
 
 private:
+    IDContainer signedKey = 0;
+    LoginMode mode = LoginMode::NoLogin;
     std::unique_ptr<Ui::WelcomePanel> ui;
 };
 
--- src/panels/welcomepanel.cpp
+++ src/panels/welcomepanel.cpp
@@ -1,15 +1,18 @@
 #include "include/panels/welcomepanel.h"
+#include "include/core/general.h"
+#include "include/core/admin.h"
+#include "include/core/user.h"
 #include "ui_welcomepanel.h"
+#include <QMessageBox>
 #include <QTimer>
 
-WelcomePanel::WelcomePanel(QWidget *parent) : QMainWindow(parent)
+WelcomePanel::WelcomePanel(QWidget *parent) : QDialog(parent)
 {
     ui = std::make_unique<Ui::WelcomePanel>();
-
     ui->setupUi(this);
-    ui->continueButton->setVisible(false);
 
-    QTimer::singleShot(2000, [&]()
+    ui->continueButton->setVisible(false);
+    QTimer::singleShot(2000, this, [&]()
     {
         ui->continueButton->setVisible(true);
     }
@@ -19,7 +22,133 @@ WelcomePanel::WelcomePanel(QWidget *parent) : QMainWindow(parent)
 WelcomePanel::~WelcomePanel()
 {}
 
+IDContainer WelcomePanel::getSignedkey() const
+{
+    return signedKey;
+}
+
+WelcomePanel::LoginMode WelcomePanel::getLoginMode() const
+{
+    return mode;
+}
+
 void WelcomePanel::goToSigninPage()
 {
     ui->pageContainer->setCurrentWidget(ui->signinPage);
+}
+
+void WelcomePanel::toggleShowLoginPassword(bool value)
+{
+    if(!value)
+    {
+        ui->loginPasswordEdit->setEchoMode(QLineEdit::Password);
+        ui->loginPasswordShowButton->setIcon(QIcon(":/images/windows/show-password.png"));
+    }
+    else
+    {
+        ui->loginPasswordEdit->setEchoMode(QLineEdit::Normal);
+        ui->loginPasswordShowButton->setIcon(QIcon(":/images/windows/hide-password.png"));
+    }
+}
+
+void WelcomePanel::toggleShowAccountPassword(bool value)
+{
+    if(!value)
+    {
+        ui->accountPasswordEdit->setEchoMode(QLineEdit::Password);
+        ui->accountPasswordShowButton->setIcon(QIcon(":/images/windows/show-password.png"));
+    }
+    else
+    {
+        ui->accountPasswordEdit->setEchoMode(QLineEdit::Normal);
+        ui->accountPasswordShowButton->setIcon(QIcon(":/images/windows/hide-password.png"));
+    }
+}
+
+void WelcomePanel::rejectCreation()
+{
+    goToSigninPage();
+    ui->accountFirstNameEdit->setText(QString());
+    ui->accountUserNameEdit->setText(QString());
+    ui->accountPasswordEdit->setText(QString());
+    ui->accountLastNameEdit->setText(QString());
+}
+
+void WelcomePanel::acceptCreation()
+{
+    User temp;
+    try
+    {
+        const QString lastName = ui->accountLastNameEdit->text();
+        const QString userName = ui->accountUserNameEdit->text();
+        const QString password = ui->accountPasswordEdit->text();
+        const QString firstName = ui->accountFirstNameEdit->text();
+        if(!temp.setFirstName(firstName))
+        {
+            throw std::runtime_error("The first name should only contain non-letter characters and not be empty.");
+        }
+        else if(!temp.setLastName(lastName))
+        {
+            throw std::runtime_error("The last name should only contain non-letter characters and not be empty.");
+        }
+
+        if(!temp.setUserName(userName))
+        {
+            throw std::runtime_error("The user name should have at least 6 characters.");
+        }
+
+        if(!temp.setPassword(password))
+        {
+            throw std::runtime_error("A strong password is at least 8 character, and has all kinds of characters.");
+        }
+    }
+    catch(std::exception &e)
+    {
+        QMessageBox::critical(this, "Error", e.what());
+        return;
+    }
+
+    temp.saveToRecord(ID::generateKey());
+    rejectCreation();
+}
+
+void WelcomePanel::goToAccountPage()
+{
+    ui->pageContainer->setCurrentWidget(ui->accountPage);
+}
+
+void WelcomePanel::accept()
+{
+    const QString &name = ui->loginUserNameEdit->text();
+    const QString &password = ui->loginPasswordEdit->text();
+    auto users = MainFolder::getUsers().entryInfoList({"*"}, QDir::AllDirs | QDir::NoDotAndDotDot);
+    auto admins = MainFolder::getAdmin().entryInfoList({"*.adf"}, QDir::AllEntries | QDir::NoDotAndDotDot);
+
+    for(const auto &file : users)
+    {
+        IDContainer number = file.baseName().toLongLong();
+        User user = User::loadFromRecord(number);
+        if(user.getUserName() == name && user.getPassword() == password)
+        {
+            mode = LoginMode::LoginAsUser;
+            signedKey = number;
+            QDialog::accept();
+            return;
+        }
+    }
+
+    for(const auto &file : admins)
+    {
+        IDContainer number = file.baseName().toLongLong();
+        Admin admin = Admin::loadFromRecord(number);
+        if(admin.getUserName() == name && admin.getPassword() == password)
+        {
+            mode = LoginMode::LoginAsAdmin;
+            signedKey = number;
+            QDialog::accept();
+            return;
+        }
+    }
+
+    QMessageBox::critical(this, "Error", "The entered user name or password is incorrect.");
 }
