f243c8890945df5e6fc123cfcb95cd18be2d87cf
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-06-06 11:53:52 +0330

    Defined the starting point of the program

--- main.cpp
+++ main.cpp
@@ -1,10 +1,42 @@
 #include "include/panels/welcomepanel.h"
+#include "include/panels/adminpanel.h"
+#include "include/panels/userpanel.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    WelcomePanel w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    std::unique_ptr<QMainWindow> window;
+
+    auto mainPanel = std::make_unique<WelcomePanel>();
+    mainPanel->open();
+
+    QObject::connect(mainPanel.get(), &QDialog::finished, [&](int result)
+    {
+        if(result == QDialog::Accepted)
+        {
+            switch(mainPanel->getLoginMode())
+            {
+                case WelcomePanel::LoginAsAdmin:
+                {
+                    window = std::make_unique<AdminPanel>(mainPanel->getSignedkey());
+                    window->show();
+                    break;
+                }
+                case WelcomePanel::LoginAsUser:
+                {
+                    window = std::make_unique<UserPanel>(mainPanel->getSignedkey());
+                    window->show();
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
+        }
+    }
+    );
+
+    return app.exec();
 }
