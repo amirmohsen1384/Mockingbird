452e32dca586a586f89d68772b3ad3ab6d296ed3
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-06-06 17:54:17 +0330

    Added some roles
    These roles are used within the user model.

--- include/core/user.h
+++ include/core/user.h
@@ -9,6 +9,17 @@ private:
     static QDir createRecord(const IDContainer &value);
 
 public:
+    enum UserRole
+    {
+        KeyRole = Qt::UserRole,
+        NameRole = Qt::DisplayRole,
+        FirstNameRole = Qt::UserRole + 1,
+        LastNameRole = Qt::UserRole + 2,
+        UserNameRole = Qt::UserRole + 3,
+        PasswordRole = Qt::UserRole + 4,
+        ModelRole = Qt::UserRole + 5,
+        KeyListRole = Qt::UserRole + 6
+    };
     static User loadFromRecord(const IDContainer &value);
     static IDs loadIDsFromRecord(const IDContainer &value);
     virtual void saveToRecord(const IDContainer &value) const override;
