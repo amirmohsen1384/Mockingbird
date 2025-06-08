13807c239648d921ea347300301e4bec8080ea73
Amir Mohsen Ghasemi <78074373+amirmohsen1384@users.noreply.github.com>
2025-06-08 17:53:19 +0330

    Improvement on the artist view
    Artist view now behaves more naturally when no source model or incomplete data is supplied.

--- src/dialogs/artistview.cpp
+++ src/dialogs/artistview.cpp
@@ -5,26 +5,49 @@
 
 void ArtistView::updateModel()
 {
+    const auto condition = mainModel != nullptr;
     ui->playlistView->setModel(mainModel);
     updateControl();
-    if(mainModel != nullptr)
+    if(condition)
     {
         Artist artist = Artist::loadFromRecord(mainModel->mainKey());
-        ui->nameLabel->setText(artist.getName());
+        if(artist.getName().isEmpty())
+        {
+            ui->nameLabel->setText("Unnamed artist");
+            setWindowTitle("Untitled - Artist Page");
+        }
+        else
+        {
+            ui->nameLabel->setText(artist.getName());
+            setWindowTitle(QString("%1 - Artist Page").arg(artist.getName()));
+        }
         ui->photoView->setImage(artist.getPhoto());
-        ui->biographyBrowser->setText(artist.getBiography());
-        setWindowTitle(QString("%1 - Artist Page").arg(artist.getName()));
+        if(artist.getBiography().isEmpty())
+        {
+            ui->biographyBrowser->setText("No biography has been provided.");
+        }
+        else
+        {
+            ui->biographyBrowser->setText(artist.getBiography());
+        }
     }
+    const auto visible = condition && mainModel->rowCount() > 0;
+    ui->notFoundLabel->setVisible(!visible);
+    ui->playlistView->setVisible(visible);
 }
 
 void ArtistView::updateControl()
 {
-    ui->nameLabel->setEnabled(mainModel != nullptr);
-    ui->photoView->setEnabled(mainModel != nullptr);
-    ui->pageContainer->setEnabled(mainModel != nullptr);
-    ui->biographyBrowser->setEnabled(mainModel != nullptr);
-    ui->nameLabel->setText(mainModel != nullptr ? "" : "Unable to know about the artist's name");
-    ui->biographyBrowser->setPlainText(mainModel != nullptr ? "" : "Unable to know about biography");
+    const auto condition = mainModel != nullptr;
+    ui->nameLabel->setEnabled(condition);
+    ui->photoView->setEnabled(condition);
+    ui->pageContainer->setEnabled(condition);
+    ui->biographyBrowser->setEnabled(condition);
+    if(!condition)
+    {
+        ui->nameLabel->setText("Unable to know about the artist's name");
+        ui->biographyBrowser->setPlainText("Unable to know about biography");
+    }
 }
 
 void ArtistView::playPlaylist(const QModelIndex &index)
